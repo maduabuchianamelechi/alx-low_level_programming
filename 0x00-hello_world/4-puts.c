2ae5ec091ff211ba718b0af151f5064d4a55198c
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-16 10:03:36 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main: Entry point
+ *
+ * Description: This C program prints with the put function.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
