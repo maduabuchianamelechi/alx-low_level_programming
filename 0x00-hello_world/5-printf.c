cf737cba7618b15795dd5fb57a667e2ffad38c0e
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-16 19:00:03 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints using the printf
+ *
+ * Return: Will always return 0
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
