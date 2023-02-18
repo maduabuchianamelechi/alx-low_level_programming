a3e3899244baaf51d5e0fac06b170906504b2cf2
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-17 19:20:20 -0800

    Print alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphebet in lower case and upper case
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch = "a";
+	char CH = "A";
+
+	/* Prints lowercase alphabets*/
+	while (ch <= "z")
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/* Prints uppercase alphabets */
+	while (CH <= "Z")
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n')
+
+	return (0);
+}
