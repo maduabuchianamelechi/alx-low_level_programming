e96b32313dcc34b0d082299615372491e90a9902
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-17 20:09:57 -0800

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single numbers 1 to 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	num = 0;
+
+	while (num <= 9)
+	{
+		putchar("%d", num);
+		num++;
+	}
+	putchar("\n");
+
+	return (0);
+}
