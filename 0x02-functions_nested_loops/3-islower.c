272d3e1a58d6e2feec0dba15ab589568293f5480
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-21 11:10:11 -0800

    Is lower?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * @c: The alphabet to check if lower or not
+ *
+ * Return: Returns 1 if lowercase
+ *		and 0 if not.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c<=122)
+		return (1);
+	return (0);
+}
