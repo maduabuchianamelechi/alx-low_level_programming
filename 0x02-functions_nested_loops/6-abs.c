b9e9e1a53ad68c1436d2529c3577ab9ec817a2ac
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-21 13:02:21 -0800

    Absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point (Returns the absolute value of n)
+ *
+ * @n: The value to check
+ *
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (c > 0)
+		n = (-1) * n;
+	return (n);
+}
