fcd9f77ba16e1648ad73710faf15da405d2c9b34
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-21 13:51:15 -0800

    Jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point (Prints every seconds of Jack bauer)
+ *
+ * Return: Returns nothing
+ */
+
+void jack_bauer(void)
+{
+	int min, hour;
+
+	if (hour = 0; hour <= 23; hour++)
+	{
+		if (min =0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
