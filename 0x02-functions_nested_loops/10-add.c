800d1eae3bd38c3444bb07edd72eeec729b9a1f9
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-21 14:21:50 -0800

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point (adds two integers a & b)
+ *
+ * @a: Integer 1
+ *
+ * @b: Integer 2
+ *
+ * Return: Returns the result of the addition
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
