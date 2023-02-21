3c1ec6c4a2e295b0b7284820f38a5b1449be0cd8
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-21 10:28:14 -0800

    Print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Prints alphabets 10 times
+ *
+ * Return: returns noothing (void)
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int cnt = 0;
+
+	if (cnt <= 10)
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	_putchar('\n');
+}
