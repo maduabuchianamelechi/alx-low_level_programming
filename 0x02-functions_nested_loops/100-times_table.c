9005ce2907ce1a0d55946365511c24f652ceca82
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-21 20:15:12 -0800

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,32 +18,31 @@ void print_times_table(int n)
 
 	if (n < 15 || n > 0)
 	{
-		for (num = 0; num <= n; ++num)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar(48);
-		}
-		for (mult = 1; mult <= n; ++mult)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			/*
-			 * put space if product is a single number
-			 * place the first digit it two numbers
-			 */
-
-			if (prod <= n)
+			for (mult = 1; mult <= n; mult++)
 			{
+				_putchar(',');
 				_putchar(' ');
+
+				prod = num * mult;
+
+				/*
+				 * put space if product is single number
+				 * place the first digit if two numbers
+				 */
+				if (prod <= n)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((prod / 10) + 48); /* gets the first digit*/
+				}
+				_putchar((prod % 10) + 48); /* gets the second digit*/
 			}
-			else
-			{
-				_putchar((prod / 10) + 48); /* gets the first digit*/
-			}
-			_putchar((prod % 10) + 48); /*gets the second digit*/
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
