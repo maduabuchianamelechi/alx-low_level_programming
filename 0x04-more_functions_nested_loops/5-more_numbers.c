b6192152f63c20d2b169f14b589dc85cb8a36e3f
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-23 19:03:25 -0800

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14, 10 times
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int ch;
+
+	for (num = 0; num <= 10; num++)
+	{
+		if (ch = 0; ch < 10; ch++)
+			_putchar((ch % 10) + '0');
+
+		_putchar('\n');
+	}
+}
