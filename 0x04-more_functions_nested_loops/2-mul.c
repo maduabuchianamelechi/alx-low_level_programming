b65f392ae1c5991e4f7f2f28977ba00a63812904
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-23 18:02:35 -0800

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - Entry poit (Multiplies two integers)
+ *
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: Returns the sum of the two integers
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
