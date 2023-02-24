d7503b5591d5256800792e2fdd942924e2eebbbd
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-23 18:11:26 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: Prints numbers 0 to 9
+ * 
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		_putchar(num);
+	_putchar('\n');
+
+	return (void);
+}
