92b52c98a240c67edb78839f66b7fc795dd905b2
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-27 17:07:50 -0800

    Puts two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Noting
+*/
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
