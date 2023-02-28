828e5bf93deb40feaabc805e12220df8808276d9
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-02-27 16:55:44 -0800

    Reverse strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int n, i;
+	char ch;
+
+	/*find string length without null char*/
+	for (n = 0; s[l] != '\0'; n++)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < n / 2; i++)
+	{
+		ch = s[i];
+		s[i] = s[n - 1 - i]; /*-1 because the array starts from 0*/
+		s[n - 1 - i] = ch;
+	}
+
+}
