2f37c2f2d1be76885bcb73060f75c5b1bda8f435
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-03-02 08:38:12 -0800

    Infinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,5 @@ char *leet(char *);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
