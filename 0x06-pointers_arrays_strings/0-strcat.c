751d40245fb564c380c8e9f0291292d931561e5a
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-03-02 06:20:40 -0800

    String concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - Appends a strings to the end of another string
+ *
+ * @dest: A pointer to the string to be concatenated upon
+ *
+ * @src: The source string to be appended to @dest
+ *
+ * Return: A pointer to the destination string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+        int ind = 0;
+        int len = 0;
+
+        while (dest[ind++])
+            len++;
+
+        for (ind = 0; src[ind]; ind++)
+            dest[len++] = src[ind];
+
+        return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+/**
+ * _putchar - Prints the character c to standard output
+ *
+ * @c: The character to be printed
+ *
+ * Return: 1 at success and -1 at error
+ */
+
+int _putchar(char c)
+{
+	int result;
+
+	result = write(1, &c, 1);
+
+	return(result);
+}
