f99d139d8338b80abee3eb930d3cb2d4174ac0d2
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-03-06 20:42:22 -0800

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chess board
+ *
+ * @a: input pointer
+ *
+ * Return: return nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+       for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+
+		_putchar(a[i / 8][i - j]);
+	}
+	_putchar('\n');
+}
