247a3e8d8de4671f88e99f14889ebf84ecb0cbe9
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-03-08 09:53:33 -0800

    Power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates the power of @x by @y
+ *
+ * @x: Integer x
+ *
+ * @y: Integer y
+ *
+ * Return: Returns the power of @x by @y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		result *= _pow_recursion(x, y - 1);
+		return (result);
+	}
+}
