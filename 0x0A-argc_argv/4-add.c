86aa25f30458216a4d6bdbfc9e89e1b5763f051e
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-03-10 10:14:00 -0800

    Add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * main - Entry point; multiplies two numbers
+ *
+ * @argc: The number of arguments
+ *
+ * @argv: A pointer to the arguments
+ *
+ * Return: Returns 0 on success and 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int result;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(i))
+			result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
