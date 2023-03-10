4c0455b7bad76c2c2894d4a91bad9d66112013cd
maduabuchianamelechi <maduabuchianamelechi@gmail.com>
2023-03-10 09:15:00 -0800

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point; returns the name of the programe
+ *
+ * @argc: Holds the number of arguments
+ *
+ * @argv: Holds pointer to the arguments
+ *
+ * Return: Always 0 at success.
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,16 @@
+/**
+ * _putchar - Prints the value c to standard output
+ *
+ * @c: The valuse to be printed
+ *
+ * Return: Returns 1 on success and -1 on error
+ */
+
+int _putchar(char c)
+{
+	int result;
+
+	result = write(1, &c, 1);
+
+	return (result);
+}
